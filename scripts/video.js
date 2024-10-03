a2eef859e8987a3f12e0453e2c285c8e825905eb
Rubaiat15 <rubaiatferdous15@gmail.com>
2024-10-03 23:39:12 +0600

    hoise

--- scripts/video.js
+++ scripts/video.js
@@ -7,12 +7,42 @@ function getTimeString(time){
     return`${hour} hour ${minutes} minutes ${remaininSeconds} seconds ago`
 }
 
+const removeActive =() =>{
+    const buttons = document.getElementsByClassName("category-btn");
+
+    for(let btn of buttons){
+        btn.classList.remove("active");
+    }
+};
+
 const loadCategories = ()=>{
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json() )
     .then((data)=> displayCategories(data.categories))
     .catch((error) =>console.log("error"));
     
+};
+
+
+const loadDetails = async (videoId)=>{
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    const res  = await fetch(url);
+    const data = await res.json();
+    displayDetails(data.video);
+
+}
+
+const displayDetails = (video)=>{
+    console.log(video);
+    const detailsContainer = document.getElementById("modal-content");
+
+    detailsContainer.innerHTML = `
+        <img src= ${video.thumbnail}/>
+        <p>${video.description}</p>
+    `
+
+    document.getElementById("showModal").click();
 }
 
 
@@ -22,16 +52,20 @@ const displayCategories = (categories)=>{
     categories.forEach( (item) => {
         console.log(item);
 
-        const button = document.createElement("button");
-        button.classList = "btn";
-        button.innerText = item.category;
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML = 
+        `
+            <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class= "btn category-btn">
+                ${item.category}
+            </button>
+        `;
 
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     });
 };
 
-const loadVideos = ()=>{
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText="")=>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json() )
     .then((data)=> displayVideos(data.videos))
     .catch((error) =>console.log("error"));
@@ -57,10 +91,43 @@ const loadVideos = ()=>{
 //     "description": "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
 // }
 
+const loadCategoryVideos = (id)=>{
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json() )
+    .then((data)=> {
+
+        removeActive();
+        const activeBtn = document.getElementById(`btn-${id}`)
+        activeBtn.classList.add("active");
+        displayVideos(data.category)
+    })
+    .catch((error) =>console.log("error"));
+    
+}
+
 const displayVideos = (videos)=>{
     const videoContainer = document.getElementById("videos");
+    videoContainer.innerHTML = "";
+
+    if(videos.length == 0){
+        videoContainer.classList.remove("grid");
+        videoContainer.innerHTML = `
+            <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+                <img src = "assets/icon.png"/>
+                <h2 class= "text-center font-bold text-xl">
+                    No Content Here
+                </h2>
+            </div>
+        `;
+        return;
+    }
+    else{
+        videoContainer.classList.add("grid");
+    }
+
     videos.forEach( (video) => {
-        console.log(video);
+        // console.log(video);
 
         const card = document.createElement("div");
         card.classList = "card card-compact"
@@ -70,7 +137,7 @@ const displayVideos = (videos)=>{
       src= ${video.thumbnail}
       alt="Shoes" />
       ${video.others.posted_date?.length==0 ? "" :
-      `<span class= "absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`}
+      `<span class= "absolute text-xs right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`}
     </figure>
   <div class="px-0 py-2 flex gap-2">
         <div>
@@ -82,7 +149,7 @@ const displayVideos = (videos)=>{
                 <p class= "text-gray-400 ">${video.authors[0].profile_name}</p>
                 ${video.authors[0].verified == true ?`<img class = "w-5"  src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png">`: " "}
             </div>
-            <p></p>
+            <p><button onclick = "loadDetails('${video.video_id}')" class = "btn btn-sm btn-error">Details</button></p>
         </div>
     
   </div>`
@@ -93,5 +160,10 @@ const displayVideos = (videos)=>{
 
 loadCategories();
 loadVideos();
+
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+    loadVideos(e.target.value);
+
+});
 
 
